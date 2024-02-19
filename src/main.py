3856cce614a66b1fbea9823bad2294aa4ced885c
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-02-19 01:11:27 -0300

    :sparkles: feat: Adds main.py file with main function to create PDF

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from controllers import DocumentController
+
+def main():
+    document_controller = DocumentController()
+    document_controller.create_pdf()
+
+if __name__ == "__main__":
+    main()
